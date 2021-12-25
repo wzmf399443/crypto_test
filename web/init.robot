c7097339652992904245ea7217985ca3498576a0
Mark Chang <e1234573123@gmail.com>
2021-12-25 23:36:06 +0800

    [mark-6] Add test case for checking the popup message is expected

--- web/init.robot
+++ web/init.robot
@@ -8,5 +8,6 @@ Resource     ${CURDIR}/lib/keywords_common.robot
 Resource     ${CURDIR}/lib/keywords_trade_spot.robot
 
 Variables    variables.py
+Variables    res/content/content_trade_spot.py
 Variables    res/location/markets_page.py
 Variables    res/location/trade_spot_page.py
---
+++ web/res/content/content_trade_spot.py
@@ -0,0 +1,6 @@
+order_type_popup_message = {
+    "Stop Loss": "A stop-loss order is to place a market order to buy or sell a coin once the last traded price reaches a specified price (stop price).",
+    "Stop Limit": "A stop-limit order is to place a limit order to buy or sell a coin once the last traded price reaches a specified price (stop price).",
+    "OCO Limit": """An OCO Limit Order consists of 2 legs: a Limit and a Stop Limit Order. When the Market Price hits the Limit Order Price, the Limit Order (leg 1) will be executed and the Stop Limit Order (leg 2) will be cancelled. \n\nConversely, if the Market Price hits the Stop Order Price first, then the Stop Limit Order (leg 2) will be executed and the Limit Order (leg 1) will be cancelled.""",
+    "OCO Market": """An OCO Market Order consists of 2 legs: a Limit and a Stop Market Order. When the Market Price hits the Limit Order Price, the Limit Order (leg 1) will be executed and the Stop Market Order (leg 2) will be cancelled. \n\nConversely, if the Market Price hits the Stop Order Price first, then the Stop Market Order (leg 2) will be executed and the Limit Order (leg 1) will be cancelled."""
+}
