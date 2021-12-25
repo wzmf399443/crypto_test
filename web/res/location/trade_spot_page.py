43a5c59beb07279dc4c2f12b2df7036ee35512a0
Mark Chang <e1234573123@gmail.com>
2021-12-25 15:49:39 +0800

    [mark-4] Add test case for checking the input auto fill is worked in Limit

--- web/res/location/trade_spot_page.py
+++ web/res/location/trade_spot_page.py
@@ -9,5 +9,8 @@ TRADE_SPOT_SYMBOL_INFO_TITLE_TEXT_24H = '//*[@id="app"]//div[@class="current-sym
 # Trade Form
 TRADE_SPOT_TRADE_FORM_BUTTON_TAB_BUY = '//*[@id="app"]//div[contains(@class,"trading-form")]//div[@class="button-tabs"]/div[text()=" Buy "]'
 TRADE_SPOT_TRADE_FORM_BUTTON_TAB_SELL = '//*[@id="app"]//div[contains(@class,"trading-form")]//div[@class="button-tabs"]/div[text()=" Sell "]'
-TRADE_SPOT_TRADE_FORM_TRADE_BLOCK_TRADE_TYPE = '//*[@id="app"]//div[@class="trade-block" and not(contains(@style,"display: none"))]//span[@class="trade-type"]'
-TRADE_SPOT_TRADE_FORM_TRADE_BLOCK_TRADE_TYPE_AMOUNT = '//*[@id="app"]//div[@class="trade-block" and not(contains(@style,"display: none"))]//span[@class="amount"]'
+TRADE_SPOT_TRADE_FORM_ORDER_TYPE_LIMIT = '//*[@id="app"]//div[@class="spot-tabs"]//div[@class="order-type-tabs"]//div[text()="Limit"]'
+TRADE_SPOT_TRADE_FORM_DISPLAY_TRADE_BLOCK = '//*[@id="app"]//div[@class="trade-block" and not(contains(@style,"display: none"))]'
+TRADE_SPOT_TRADE_FORM_TRADE_BLOCK_TRADE_TYPE = f'{TRADE_SPOT_TRADE_FORM_DISPLAY_TRADE_BLOCK}//span[@class="trade-type"]'
+TRADE_SPOT_TRADE_FORM_TRADE_BLOCK_TRADE_TYPE_AMOUNT = f'{TRADE_SPOT_TRADE_FORM_DISPLAY_TRADE_BLOCK}//span[@class="amount"]'
+TRADE_SPOT_TRADE_FORM_TRADE_BLOCK_TRADE_INPUT = f'{TRADE_SPOT_TRADE_FORM_DISPLAY_TRADE_BLOCK}//div[@class="trade-input"]/div[@class="input-item"]'
